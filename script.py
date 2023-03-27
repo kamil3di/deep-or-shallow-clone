472cd4d37dea4c8cb2377c6169a9a1946820a872
Kamil Dinleyici <kamildinleyici@hotmail.com>
2023-03-27 16:25:22 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,27 +1,28 @@
 import os
 
-folder_path = "path/to/folder"  # ana klasör yolunu belirleyin
-delta_logs_file = "delta_logs.txt"  # delta_log klasörleri dosyasının adını belirleyin
-parquets_file = "parquets.txt"  # parquet klasörleri dosyasının adını belirleyin
+folder_path = "path/to/folder"  # ana klasör
+delta_logs_file = "delta_logs.txt"  # delta_log klasörleri dosyasının adı
+parquets_file = "parquets.txt"  # parquet klasörleri dosyasının adı
 
-delta_logs = []  # tüm delta_log klasörlerinin tam yolunu tutacak bir liste oluşturun
-parquets = []  # tüm parquet klasörlerinin tam yolunu tutacak bir liste oluşturun
+delta_logs = []  # tüm delta_log klasörlerinin tam yolunu tutacak bir liste
+parquets = []  # tüm parquet klasörlerinin tam yolunu tutacak bir liste
+delta_path = "_delta_log"
 
 for root, dirs, files in os.walk(folder_path):
-    if "delta_log" in dirs:
+    if delta_path in dirs:
         delta_logs.append(os.path.join(root))
     else:
         if not "delta_log" in root:
             parquets.append(root)
 
-# delta_log ve parquet klasörlerinin tam yolunu ilgili dosyalara kaydedin
+# delta_log ve parquet klasörlerinin tam yolunu ilgili dosyalara kaydetme işlemi
 with open(delta_logs_file, "w") as f:
     f.write("\n".join(delta_logs))
 
 with open(parquets_file, "w") as f:
     f.write("\n".join(parquets))
 
-# SQL sorgularını hazırlayın ve ilgili dosyaya kaydedin
+# SQL sorgularını hazırlayın ve ilgili dosyaya kaydetme işlemi
 with open(delta_logs_file, "r") as f:
     delta_logs = f.readlines()
 
